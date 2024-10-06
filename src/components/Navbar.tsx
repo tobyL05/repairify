47a522ece21d3d6b3ca308c03525b0f1e61fbf53
Toby <toby.lau075@gmail.com>
2024-10-05 18:19:04 -0700

    fixed styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
+import { FaScrewdriverWrench } from "react-icons/fa6"
 
 export default function Navbar() {
 
     return (
         <div className="w-full h-10 my-5">
             <div className="w-full flex justify-between align-baseline">
-                <h1 className="text-2xl">repairify</h1>
+                <div className="inline-flex align-baseline"><h1 className="text-2xl">repairify</h1> <FaScrewdriverWrench/></div>
                 <h1>reducing e-waste one device at a time</h1>
             </div>
         </div>
